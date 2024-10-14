ad286a9bc5f22c1b5b38cd7b78379bac54f33f96
Luis Angel Perez Ortiz <ruisu_3912@hotmail.com>
2024-10-13 18:21:05 -0600

    Cambios en subgrafos

--- subgrafos.py
+++ subgrafos.py
@@ -8,7 +8,7 @@ e = [(1, 2), (1, 3), (2, 3),
      (3, 8), (4, 5), (5, 6),
      (6, 7), (7, 8), (8, 1)]
 
-G1 = add_edges_from(e)
+G1.add_edges_from(e)
 
 vs = [1, 2, 3, 4, 5]
 subgraf = G1.subgraph(vs)
